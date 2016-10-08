22e77340a457b8ca2d61d5b38e05630425ca5026
victor kushch <victor@ku-64.com>
2016-10-08 12:46:54 -0400

    Draving Enemies and Player

--- js/app.js
+++ js/app.js
@@ -1,32 +1,96 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Game = function() {
+	this.gameOver = false;
+	this.gameWin = false;
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Enemies our player must avoid
+var Enemy = function(x,y) {
+
+    //setting image for enemy
     this.sprite = 'images/enemy-bug.png';
+
+    //setting position
+
+    this.x = x;
+    this.y = y;
+
+
+
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x = this.x + 101;
+    if (this.x > 505){
+        this.reset();
+    }
 };
+// reset enemy to the left side with 3 random y options
+Enemy.prototype.reset = function() {
+    this.x = -200;
+    var yValues = [60, 145, 230];
+    this.y = yValues[Math.floor(Math.random()*3)];
+}
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//
+//  PLAYER CLASS
+//
+
+//Player constructor
+
+var Player = function(x,y){
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+
+    this.x_original_position = x;
+    this.y_original_position = y;
+
+
+}
+
+Player.prototype.update = function(){
+    this.x = this.x;
+    this.y = this.y;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
 // Now instantiate your objects.
+
+/*
+*
+* INSTANTIONATE ALL OBJECTS
+*
+*/
+
+var allEnemies = [];
+var y = 145;
+var x = -100;
+
+var enemy = new Enemy(x, y);
+
+allEnemies.push(enemy);
+
+
+
+var player = new Player(303, 380);
+
+var Game = new Game();
+
+
+
+
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
